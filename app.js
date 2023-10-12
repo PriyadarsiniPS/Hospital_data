21c01f9c5a9e8ee781e5175068e102ea879747e2
PriyadarsiniPS <priyapremlal2015@gmail.com>
2023-10-12 05:36:42 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express()
+app.use(express.json());
+require('dotenv').config();
+const routerfile=require('./routes/basic');
+app.use('/api',routerfile);
+const port=process.env.port;
+app.listen(port,()=>{
+    console.log(`server is running in ${port}`);
+})
